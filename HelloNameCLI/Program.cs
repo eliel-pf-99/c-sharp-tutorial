8a184fb64514f7852f1cddfbb781287ab1655006
Eliel P. F. <eliel.dev.99@gmail.com>
2024-04-14 16:44:19 -0300

    Update HelloNameCLI/Program.cs

--- HelloNameCLI/Program.cs
+++ HelloNameCLI/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; // import System library.
+
+namespace HelloNameCLI // namespace declaration.
+{
+    class Program // Program class declaration.
+    {
+        static void Main(string[] args) // Main method is the entry-point of the application, it's essencial for executing the program.
+        {
+            Console.WriteLine("Please enter your name: "); // Print to the console what is inside the quotes. ("") 
+
+            var name = Console.ReadLine(); // Reads what the user enters into the console.
+
+            Console.WriteLine($"Hello {name}"); // Print to the console what is inside the quotes with interpolatio, with the user's response.
+
+            Console.ReadKey(); // Keeps the program running...
+        }
+    }
+}
