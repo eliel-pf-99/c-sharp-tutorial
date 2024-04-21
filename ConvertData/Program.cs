7bfe38d22754ddf07740853f2b901aa7f1d1433f
Eliel P. F. <eliel.dev.99@gmail.com>
2024-04-20 21:36:05 -0300

    ConvertData Program up

--- ConvertData/Program.cs
+++ ConvertData/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+
+    struct ImperialMeasurment
+    {
+        public float feet;
+
+        public ImperialMeasurment(float r)
+        {
+            this.feet = r;
+        }
+
+        public static implicit operator ImperialMeasurment(int m) 
+        {
+            float convertionResult = 3.28f * m;
+            ImperialMeasurment temp = new ImperialMeasurment(convertionResult);
+            return temp;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a whole number measuremnt in meters");
+
+        int mm = Convert.ToInt32(Console.ReadLine());
+
+        ImperialMeasurment im = mm;
+
+        Console.WriteLine($"This measurment of {mm} in meters is {im.feet} in feet.");
+
+        Console.ReadKey();
+    }
+}
